009be8d6a4ea79b0adad40c79b0fc8e88ce80861
Alberto Calvo <albertocalvojaca@gmail.com>
2020-12-05 12:44:41 +0100

    Max command

--- src/main/java/soa/eip/Router.java
+++ src/main/java/soa/eip/Router.java
@@ -1,5 +1,6 @@
 package soa.eip;
 
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +11,34 @@ public class Router extends RouteBuilder {
 
   @Override
   public void configure() {
+
+    Processor maxProcessor = exchange -> {
+      String body = exchange.getIn().getBody(String.class),
+          newBody = "",
+          max = "";
+      for (String token : body.split(" ")) {
+        if (token.matches("max:[0-9]+")) {
+          max = token.split(":")[1];
+        } else {
+          newBody += token + " ";
+        }
+        exchange.getIn().setBody(newBody);
+        exchange.getIn().setHeader("count", max);
+      }
+    };
+
     from(DIRECT_URI)
       .log("Body contains \"${body}\"")
       .log("Searching twitter for \"${body}\"!")
-      .toD("twitter-search:${body}")
+      .choice()
+        .when(body().regex(".*max:[0-9]+.*"))
+        .process(maxProcessor)
+        .toD("twitter-search:${body}?count=${header.count}")
+        .endChoice()
+      .otherwise()
+        .toD("twitter-search:${body}")
+        .endChoice()
+        .end()
       .log("Body now contains the response from twitter:\n${body}");
   }
 }
